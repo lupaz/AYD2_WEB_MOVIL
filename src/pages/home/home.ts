89d12b8efde0816b9fa656d0d676c46c170ae6e4
lupaz <eslupaz@hotmail.com>
2018-08-16 08:12:59 -0600

    Se modifico el proyecto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegistroPage } from '../registro/registro';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  usuario:String;
+  pass:String;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ingresar(){
+    console.log("Usuario: "+this.usuario);
+    console.log("Contaseña: "+this.pass);
+  }
+
+  registrar(){
+    this.navCtrl.push(RegistroPage);
   }
 
 }
